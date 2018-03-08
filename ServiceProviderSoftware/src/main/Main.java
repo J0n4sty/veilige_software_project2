0c79f2907aa41126e49160bb91fb78d83c34fde2
Jonas Vandenberghe <jonas.vandenberghe1@student.kuleuven.be>
2018-03-08 17:03:27 +0100

    Main service providers

---
+++ ServiceProviderSoftware/src/main/Main.java
@@ -0,0 +1,29 @@
+package main;
+
+import serviceproviders.Default;
+import serviceproviders.EGov;
+import serviceproviders.Post;
+import serviceproviders.SocNet;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Default defaultDomain1 = new Default();
+		Default defaultDomain2 = new Default();
+		
+		EGov eGovDomain1 = new EGov();
+		EGov eGovDomain2 = new EGov();
+		
+		Post postDomain1 = new Post();
+		Post postDomain2 = new Post();
+		
+		SocNet socNetDomain1 = new SocNet();
+		SocNet socNetDomain2 = new SocNet();
+		
+		
+		
+	}
+
+}
